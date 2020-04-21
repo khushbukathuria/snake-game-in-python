cf3a9c19578016e8fee2a7ccc47e358b12e2c89c
khushbukathuria <khushbuk2k@gmail.com>
2020-04-21 12:08:51 +0530

    made changes to make the screen retain

--- snakegame.py
+++ snakegame.py
@@ -2,5 +2,10 @@ import pygame
 pygame.init()
 dis=pygame.display.set_mode((400,300))
 pygame.display.update()
+pygame.display.set_caption('Snake Game')
+game_over=False
+while not game_over:
+    for event in pygame.event.get():
+        print(event)    #this command prints out all the events 
 pygame.quit()
 quit()
